7b1ae9d5e5f0c12f3c5e6401bb97fd8ebb6fc40a
Deven Phillips <deven.phillips@gmail.com>
2017-04-29 20:17:30 -0400

    Cleaned up and added comments

--- src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
+++ src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
@@ -7,6 +7,9 @@ import io.vertx.core.logging.LoggerFactory;
 
 import static java.util.UUID.randomUUID;
 
+/**
+ * A very simple Verticle which just has a timed event firing with a different UUID for each run.
+ */
 public class MainVerticle extends AbstractVerticle {
 
     private static final String UUID = randomUUID().toString();
@@ -14,9 +17,12 @@ public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
+
+        // Schedule the timerFired method to run every 2 seconds + a randomized delay.
         long delay = Math.round(Math.random()*100 + 2000);
         vertx.setPeriodic(delay, this::timerFired);
 
+        // Configure a consumer to read events from the event bus and print them to the console.
         vertx.eventBus().consumer("hello.address", this::handleMessage);
     }
 
