87f424f703061c76a3fe2c1721746843fcc2a99a
Deven Phillips <deven.phillips@redhat.com>
2017-09-20 15:37:10 -0400

    Work in progress on OCP cluster

--- src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
+++ src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
@@ -4,6 +4,8 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 import static java.util.UUID.randomUUID;
 
@@ -12,11 +14,18 @@ import static java.util.UUID.randomUUID;
  */
 public class MainVerticle extends AbstractVerticle {
 
-    private static final String UUID = randomUUID().toString();
+    private static final String UUID = System.getenv("HOSTNAME");
     private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
     @Override
     public void start() {
+
+        Router router = Router.router(vertx);
+
+        router.route().handler(StaticHandler.create("webroot"));
+        vertx.createHttpServer().requestHandler(router::accept).listen(8778);
+
+
 
         // Schedule the timerFired method to run every 2 seconds + a randomized delay.
         long delay = Math.round(Math.random()*100 + 2000);
